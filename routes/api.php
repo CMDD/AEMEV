6678c7b7b701de4889535c7cdb8955f4d8d7c54b
John Alvaro <jhon54plex@hotmail.com>
2019-10-23 14:13:59 -0500

    Agregando videos

--- routes/api.php
+++ routes/api.php
@@ -36,8 +36,8 @@ Route::get('get-dia/{id}/{oracional}','Puerta\PuertaController@getDia');
 //Adultos
 Route::post('crear-dia-adultos','Adulto\DiaController@store');
 Route::get('get-dias-Adultos/{id}','Adulto\DiaController@getDias');
-Route::get('get-admin-dia-adultos/{id}','Adulto\DiaController@getDia');
-Route::get('get-dia-adultos/{id}/{oracional}','Adulto\DiaController@getDiaAdmin');
+Route::get('get-admin-dia-adultos/{id}','Adulto\DiaController@getDiaAdmin');
+Route::get('get-dia-adultos/{id}/{oracional}','Adulto\DiaController@getDia');
 Route::post('update-dia-adultos','Adulto\DiaController@update');
 
 
