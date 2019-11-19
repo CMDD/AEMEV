41d2025cdb71ab0690ecff68740465f8ac82f517
John Alvaro <jhon54plex@hotmail.com>
2019-11-19 17:37:51 -0500

    Agregando camcbios para suscripciones

--- app/Http/Controllers/SuscripcionController.php
+++ app/Http/Controllers/SuscripcionController.php
@@ -30,6 +30,6 @@ class SuscripcionController extends Controller
 
     public function getOracionales($nombre){
         
-        return Oracional::where('nombre',$nombre)->get();
+        return Oracional::where('nombre',$nombre)->orderBy('id', 'DESC')->get();
     }
 }
