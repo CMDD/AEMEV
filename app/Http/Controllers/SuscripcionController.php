1ee91b037507fe70b0ed90e3387ad703a9fb91e4
John Alvaro <jhon54plex@hotmail.com>
2019-11-20 19:34:03 -0500

    Agregando cambios

--- app/Http/Controllers/SuscripcionController.php
+++ app/Http/Controllers/SuscripcionController.php
@@ -10,22 +10,22 @@ class SuscripcionController extends Controller
 
     public function store(Request $request){
 
-        $result = Ssuscripcion::where('oracional_nombre',$request->oracional['nombre'])
+        $result = Suscripcion::where('oracional_nombre',$request->oracional['nombre'])
                 ->where('user_id',$request->id)->first();
 
          if (!$result) {
-        $suscripcion = new Suscripcion();
-        $suscripcion->precio = '';
-        $suscripcion->state = true;
-        $suscripcion->plan = '1 año';
-        $suscripcion->nombre_oracional = $request->oracional['nombre'];
-        $suscripcion->portada_oracional = $request->oracional['portada'];
-        $suscripcion->mes_oracional = $request->oracional['mes'];
-        $suscripcion->ano = $request->oracional['ano'];
-        $suscripcion->oracional_id = $request->oracional['id'];
-        $suscripcion->user_id =  $request->id;
-        $suscripcion->save();
-        return response()->json(['status'=>200]);
+            $suscripcion = new Suscripcion();
+            $suscripcion->precio = '';
+            $suscripcion->state = true;
+            $suscripcion->plan = '1 año';
+            $suscripcion->nombre_oracional = $request->oracional['nombre'];
+            $suscripcion->portada_oracional = $request->oracional['portada'];
+            $suscripcion->mes_oracional = $request->oracional['mes'];
+            $suscripcion->ano = $request->oracional['ano'];
+            $suscripcion->oracional_id = $request->oracional['id'];
+            $suscripcion->user_id =  $request->id;
+            $suscripcion->save();
+            return response()->json(['status'=>200]);
          }       
 
          return response()->json(['status'=>100]);
